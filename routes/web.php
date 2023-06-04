6775ff382fb0b0ac5625c20eebaac792ca1d1a85
damirpie <damirdamir692@gmail.com>
2023-06-04 23:05:48 +0600

    p2 3:23

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [\App\Http\Controllers\MainController::class, 'index']);
+Route::get('/categories', [\App\Http\Controllers\MainController::class, 'categories']);
+Route::get('/mobiles/iphone_x_64', [\App\Http\Controllers\MainController::class, 'product']);
 
-Route::get('/categories', function () {
-    return view('categories');
-});
-
-Route::get('/mobiles/iphone_x_64', function () {
-    return view('product');
-});
